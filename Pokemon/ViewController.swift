4ce63d2edddf203508612189e66baa6b53dcd69c
Güney Köse <guneykose@volvoxmobile.com>
2023-03-20 18:50:25 +0300

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Güney Köse on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
